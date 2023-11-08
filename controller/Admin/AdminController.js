79953beb8b99f65144de1d3f1156fec162fd49a9
Kulweet Kumar <kulweetkumar@cqlsys.co.uk>
2023-11-08 14:46:10 +0530

    fds

--- controller/Admin/AdminController.js
+++ controller/Admin/AdminController.js
@@ -7,7 +7,7 @@ const { Validator } = require('node-input-validator');
 
 module.exports = {
 
-    loginPage: async(req, res)=> {
+    loginPage: async (req, res) => {
         try {
             res.render('Admin/admin/loginPage')
         } catch (error) {
@@ -15,12 +15,12 @@ module.exports = {
         }
     },
 
-    login: async(req, res)=> {
+    login: async (req, res) => {
         try {
-            var findUser = await userModel.findOne({email: req.body.email,role:"0"})
+            var findUser = await userModel.findOne({ email: req.body.email, role: "0" })
             if (findUser) {
                 let checkPassword = await bcrypt.compare(req.body.password, findUser.password);
-               
+
                 if (checkPassword == true) {
                     req.session.user = findUser;
 
@@ -30,74 +30,126 @@ module.exports = {
                     // console.log("incorrect password")
                     // res.json("incorrect password")
                     res.redirect("/loginPage")
-                } 
-            }   else {
-                    console.log("incorrect email")
-                    // res.json("incorrect email")
-                    res.redirect("/loginPage")
-
                 }
+            } else {
+                console.log("incorrect email")
+                // res.json("incorrect email")
+                res.redirect("/loginPage")
+
+            }
         } catch (error) {
             console.log(error)
         }
     },
 
-    dashboard: async(req, res)=> {
+    dashboard: async (req, res) => {
         try {
             let title = "dashboard"
-            let users = await userModel.count({role:1})
+            let users = await userModel.count({ role: 1 })
             let babies = await babyModel.count()
             let subscription = await subscriptions.count()
-            res.render('Admin/admin/dashboard', {title, users, babies, subscription, session:req.session.user,  msg: req.flash('msg')})
+            const baby = await babyModel.aggregate([
+                {
+                    $project: {
+                        month: { $month: "$createdAt" },
+                        year: { $year: "$createdAt" }
+                    }
+                },
+                {
+                    $group: {
+                        _id: { month: "$month", year: "$year" },
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    $sort: {
+                        "_id.year": 1, "_id.month": 1
+                    }
+                }
+            ]);
+            const babyCount = Array.from({ length: 12 }).fill(0);
+           await baby.forEach(babyDAta => {
+                const monthIndex = babyDAta._id.month - 1;
+                babyCount[monthIndex] = babyDAta.count;
+            });
+            const results = await userModel.aggregate([
+                {
+                    $match: { role: "1" }
+                },
+                {
+                    $project: {
+                        month: { $month: "$createdAt" },
+                        year: { $year: "$createdAt" }
+                    }
+                },
+                {
+                    $group: {
+                        _id: { month: "$month", year: "$year" },
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    $sort: {
+                        "_id.year": 1, "_id.month": 1
+                    }
+                }
+            ]);
+            const userCounts = Array.from({ length: 12 }).fill(0);
+           await results.forEach(result => {
+                const monthIndex = result._id.month - 1;
+                userCounts[monthIndex] = result.count;
+            });
+            res.render('Admin/admin/dashboard', { userCounts,babyCount, title, users, babies, subscription, session: req.session.user, msg: req.flash('msg') })
         } catch (error) {
             console.log(error)
         }
     },
 
-    profile: async(req, res)=> {
+    profile: async (req, res) => {
         try {
             let title = "profile"
-            res.render('Admin/admin/profile', {title, session:req.session.user,  msg: req.flash('msg')})
+            res.render('Admin/admin/profile', { title, session: req.session.user, msg: req.flash('msg') })
         } catch (error) {
             console.log(error)
         }
     },
 
-    editprofile: async(req, res)=> {
+    editprofile: async (req, res) => {
         try {
             let title = "editprofile"
-            res.render('Admin/admin/editprofile', {title, session:req.session.user,  msg: req.flash('msg')})
+            res.render('Admin/admin/editprofile', { title, session: req.session.user, msg: req.flash('msg') })
         } catch (error) {
             console.log(error)
         }
     },
 
-    updateAdminProfile: async(req, res)=> {
+    updateAdminProfile: async (req, res) => {
         try {
-            if (req.files && req.files.image){
+            if (req.files && req.files.image) {
                 var image = req.files.image;
 
-                if(image){
+                if (image) {
                     req.body.image = helper.imageUpload(image, "images")
                 }
             }
-            const updateData = await userModel.findByIdAndUpdate({ _id: req.session.user._id},
-                
-                {   name: req.body.name,
+            const updateData = await userModel.findByIdAndUpdate({ _id: req.session.user._id },
+
+                {
+                    name: req.body.name,
                     phone: req.body.phone,
                     email: req.body.email,
                     image: req.body.image
                 });
 
 
-            const data = await userModel.findById({_id: req.session.user._id})
+            const data = await userModel.findById({ _id: req.session.user._id })
             req.session.user = data;
-                // res.json("updated successfully")
+            // res.json("updated successfully")
 
-            if (updateData){
+            if (updateData) {
                 res.redirect("back");
                 // res.json("error")
-            }else {
+            } else {
                 res.redirect("back");
                 // res.json("failed")
             }
@@ -105,14 +157,14 @@ module.exports = {
             res.json("here")
             // res.redirect("/profile")
         } catch (error) {
-           console.log(error) 
+            console.log(error)
         }
     },
 
-    changePassword: async(req, res)=> {
+    changePassword: async (req, res) => {
         try {
             let title = "changePassword"
-            res.render('Admin/admin/changePassword', {title, session:req.session.user,  msg: req.flash('msg')})
+            res.render('Admin/admin/changePassword', { title, session: req.session.user, msg: req.flash('msg') })
         } catch (error) {
             console.log(error)
         }
@@ -121,49 +173,49 @@ module.exports = {
     updatepassword: async function (req, res) {
         try {
             const V = new Validator(req.body, {
-            oldPassword: "required",
-            newPassword: "required",
-            confirm_password: "required|same:newPassword",
-          });
-  
-          V.check().then(function (matched) {
-            console.log(matched);
-            console.log(V.errors);
-          });
-          let data = req.session.user;
-  
-          if (data) {
-              let comp = await bcrypt.compare(V.inputs.oldPassword, data.password);
-              
-              if (comp) {
-                  const bcryptPassword = await bcrypt.hash(req.body.newPassword, 10);
-                  let create = await userModel.updateOne(
-                      { _id: data._id },
-                      { password: bcryptPassword }
-                      );
-                      req.session.user = create;
+                oldPassword: "required",
+                newPassword: "required",
+                confirm_password: "required|same:newPassword",
+            });
+
+            V.check().then(function (matched) {
+                console.log(matched);
+                console.log(V.errors);
+            });
+            let data = req.session.user;
+
+            if (data) {
+                let comp = await bcrypt.compare(V.inputs.oldPassword, data.password);
+
+                if (comp) {
+                    const bcryptPassword = await bcrypt.hash(req.body.newPassword, 10);
+                    let create = await userModel.updateOne(
+                        { _id: data._id },
+                        { password: bcryptPassword }
+                    );
+                    req.session.user = create;
                     //   console.log(create, ">>>>>>>>>.");return
-            //   req.flash('msg', 'Update password successfully')
-              res.redirect("/loginPage");
-            // res.json("updated successfully")
-            } else {
-            //   req.flash('msg', 'Old password do not match')
-              res.redirect("/changePassword");
-            // res.json("Old password do not match")
+                    //   req.flash('msg', 'Update password successfully')
+                    res.redirect("/loginPage");
+                    // res.json("updated successfully")
+                } else {
+                    //   req.flash('msg', 'Old password do not match')
+                    res.redirect("/changePassword");
+                    // res.json("Old password do not match")
+                }
             }
-          }
-      } catch (error) {
-        console.log(error) 
-      }
+        } catch (error) {
+            console.log(error)
+        }
     },
 
     logout: async (req, res) => {
         try {
-          req.session.destroy((err) => { });
-          res.redirect("/loginPage");
+            req.session.destroy((err) => { });
+            res.redirect("/loginPage");
         } catch (error) {
-          helper.error(res, error);
+            helper.error(res, error);
         }
-      },
-    
+    },
+
 }
