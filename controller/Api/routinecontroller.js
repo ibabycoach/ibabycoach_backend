874850b3843d7c78be38b1d8024c6038f83c6a1f
Ankush <ankushmanhar@cqlsys.co.uk>
2024-01-05 18:48:09 +0530

    updated chat

--- controller/Api/routinecontroller.js
+++ controller/Api/routinecontroller.js
@@ -5,51 +5,57 @@ const { Validator } = require('node-input-validator');
 
 module.exports = {
 
-    add_routine: async(req, res)=> {
-        try {
-            const v = new Validator(req.body, {
-                activityId: "required",
-                babyId: "required",
-                day: "required",
-                time: "required"
-              });
-        
-              const errorResponse = await helper.checkValidation(v);
-              if (errorResponse) {
-                return helper.failed(res, errorResponse);
-              }
-              let userId = req.user.id;
-              const addroutine = await routinebuilder.create({ 
-                userId,
-                ...req.body
-              })
-
-              return helper.success(res, "routine added successfully", addroutine)
-            
-        } catch (error) {
-            console.log(error``)
-        }
-    },
-
-    edit_routine: async(req, res)=> {
+  add_routine: async(req, res)=> {
       try {
-        const v = new Validator(req, res, {
+        const v = new Validator(req.body, {
+          activityId: "required",
+          babyId: "required",
+          day: "required",
+          time: "required"
+        });
+        
+        const errorResponse = await helper.checkValidation(v);
+        if (errorResponse) {
+          return helper.failed(res, errorResponse);
+        }
+        let userId = req.user.id;
+        const addroutine = await routinebuilder.create({ 
+          userId,
+          ...req.body
+        })
+
+        return helper.success(res, "routine added successfully", addroutine)
+            
+      } catch (error) {
+        console.log(error``)
+      }
+  },
+
+  edit_routine: async(req, res)=> {
+      try {
+        
+        const v = new Validator(req.body, {
           routineId: "required"
         })
         const errorResponse = await helper.checkValidation(v);
         if (errorResponse) {
           return helper.failed(res, errorResponse);
         }
-
+        
         let routineId = req.body.routineId;
         const updateroutine = await routinebuilder.findByIdAndUpdate({_id: req.body.routineId},
-          {...req.body});
-
-          return helper.success(res, "routine updated successfully")
+        {
+          ...req.body
+        });
+        
+        const updatedroutine = await routinebuilder.findOne({_id: req.body.routineId})
+        return helper.success(res, "routine updated successfully", updatedroutine )
 
       } catch (error) {
         console.log(error);
       }
-    }
+  }
+
+
 
 }
