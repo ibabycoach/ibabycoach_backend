9d6bd68ff19c460d759ed43749bec8ff932c1aab
Ankush <ankushmanhar@cqlsys.co.uk>
2024-02-13 12:51:08 +0530

    updated code

--- controller/Api/goalcontroller.js
+++ controller/Api/goalcontroller.js
@@ -26,21 +26,25 @@ module.exports = {
                 return helper.failed(res, "Validation error");
             }
     
-            let query = {};
-            
+            let query = { deleted: false };
+    
             if (req.body.day_name) {
-                // If the day is specified, add it to the query
-                query.day = req.body.day_name;
+                // If the day is specified, add it to the query using regex
+                const specifiedDay = req.body.day_name;
+                query.day = { $regex: new RegExp(`\\b${specifiedDay}\\b`, 'i') }; // Match whole word, case-insensitive
             }
     
-            const goals = await goalModel.find(query);
+            const goalslist = await goalModel.find(query);
     
-            return helper.success(res, "Goals", goals);
+            return helper.success(res, "Goals list", goalslist);
         } catch (error) {
             console.log(error);
             return helper.failed(res, "Error occurred while fetching goals");
         }
     }
+    
+    
+    
     
     
 
