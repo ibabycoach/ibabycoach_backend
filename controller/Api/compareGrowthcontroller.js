b183c161b6edf55ca107245c1e05367d1768654c
Ankush <youremail@example.com>
2024-10-23 09:56:07 +0000

    admindeletesoftdelete

--- controller/Api/compareGrowthcontroller.js
+++ controller/Api/compareGrowthcontroller.js
@@ -6,35 +6,6 @@ const { Validator } = require('node-input-validator');
 
 module.exports = {
 
-    // compareGrowths: async (req, res) => {
-    //     try {
-    //         const v = new Validator(req.body, {
-    //             growthId: "required",
-    //             compareId: "required"
-    //         });
-    //         const errorResponse = await helper.checkValidation(v);
-    //         if (errorResponse) {
-    //             return helper.failed(res, errorResponse);
-    //         }
-    
-    //         const babyGrowthData = await growthModel.findOne({ _id: req.body.growthId }).populate('babyId', 'age');
-    //         const babyId = babyGrowthData.babyId;
-    //         const babyData = await babyModel.findOne({ _id: babyId });
-    //         const babyDOB = babyData.birthday;
-    //         const babyAgeInWeeks = Math.floor((new Date() - new Date(babyDOB)) / (7 * 24 * 60 * 60 * 1000));
-    
-    //         const compareDataWith = await comparegrowth.findOne({ _id: req.body.compareId });
-    //         return res.status(200).json({
-    //             message: "Growth data fetched successfully",
-    //             babyAgeInWeeks: babyAgeInWeeks,
-    //             babyGrowthData,
-    //             compareDataWith
-    //         });
-    //     } catch (error) {
-    //         return helper.failed(error);
-    //     }
-    // },
-
     compareGrowths: async (req, res) => {
     try {
         const v = new Validator(req.body, {
--- model/Admin/growth.js
+++ model/Admin/growth.js
@@ -4,36 +4,53 @@ const growthSchema = new mongoose.Schema({
   userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
   babyId: {type: mongoose.Schema.Types.ObjectId, ref: "baby"},
   time: { type: Date, default: '' },
-  height: { type: String, default: '0' },
-  weight: { type: String, default: '0' },
-  oz: { type: String, default: '0' },
-  headSize: { type: String, default: '0' },
-  height_unit: {
-    type: String,
-    enum: ['in', 'cm'], 
-    default: 'in'
-  },
-  weight_unit: {
-    type: String,
-    enum: ['kg', 'lb, oz', 'lbs'], 
-    default: 'lbs'
-  },
-  oz_unit: {type: String, default: '0'},
-  headSize_unit: {
-    type: String,
-    enum: ['in', 'cm'], 
-    default: 'in'
-  },
-  lastHeight: { type: String, default: '0' },
-  lastWeight: { type: String, default: '0' },
-  last_oz: { type: String, default: '0' },
-  lastHeadSize: { type: String, default: '0' },
 
-  lastHeight_unit: { type: String, default: '0' },
-  lastWeight_unit: { type: String, default: '0' },
-  lastHeadSize_unit: { type: String, default: '0' },
+  height_in_inch: { type: String, default: '' },
+  height_in_cm: { type: String, default: '' },
+  height_difference_in_inch: { type: String, default: '' },
+  height_difference_in_cm: { type: String, default: '' },
 
-  image: { type: String, default: '' },
+  weight_in_kg: { type: String, default: '' },
+  weight_in_lbs: { type: String, default: '' },
+  weight_difference_in_lbs: { type: String, default: '' },
+  weight_difference_in_kg: { type: String, default: '' },
+
+  headSize_in_inch: { type: String, default: '' },
+  headSize_in_cm: { type: String, default: '' },
+  headSize_difference_in_inch: { type: String, default: '' },
+  headSize_difference_in_cm: { type: String, default: '' },
+
+
+  // height: { type: String, default: '0' },
+  // weight: { type: String, default: '0' },
+  // oz: { type: String, default: '0' },
+  // headSize: { type: String, default: '0' },
+  // height_unit: {
+  //   type: String,
+  //   enum: ['in', 'cm'], 
+  //   default: 'in'
+  // },
+  // weight_unit: {
+  //   type: String,
+  //   enum: ['kg', 'lb, oz', 'lbs'], 
+  //   default: 'lbs'
+  // },
+  // oz_unit: {type: String, default: '0'},
+  // headSize_unit: {
+  //   type: String,
+  //   enum: ['in', 'cm'], 
+  //   default: 'in'
+  // },
+  // lastHeight: { type: String, default: '0' },
+  // lastWeight: { type: String, default: '0' },
+  // last_oz: { type: String, default: '0' },
+  // lastHeadSize: { type: String, default: '0' },
+
+  // lastHeight_unit: { type: String, default: '0' },
+  // lastWeight_unit: { type: String, default: '0' },
+  // lastHeadSize_unit: { type: String, default: '0' },
+
+  // image: { type: String, default: '' },
  
   deleted: {type: Boolean, default: false},
 }, 
