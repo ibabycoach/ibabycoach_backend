2d09c4cad82ecbbd8d3e2cb9791c57f46183a8d5
cql60 <ankushmanhar@cqlsys.co.uk>
2025-09-10 09:41:30 +0000

    caregiver listing

--- controller/Api/userprofilecontroller.js
+++ controller/Api/userprofilecontroller.js
@@ -134,7 +134,9 @@ module.exports = {
   subUser_list: async (req, res) => {
     try {
       const userId = req.user._id
-      let sub_user_Data = await user_model.find({ parentId: userId, deleted: false}).sort({ createdAt: -1 }).populate("parentId", 'name')
+      // let sub_user_Data = await user_model.find({ parentId: userId, deleted: false}).sort({ createdAt: -1 }).populate("parentId", 'name')
+
+      let sub_user_Data = await caregiverModel.find({ userId: userId, deleted: false}).sort({ createdAt: -1 }).populate("userId", 'name')
 
       if (!sub_user_Data) {
         return helper.failed(res, "Sub-user not found");
